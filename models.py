bbc6cd523ba2c2de925fadb579494b85529e7b5b
Jakub Cisło <cubix651@gmail.com>
2020-11-07 22:22:37 +0100

    Keep solution clues

--- models.py
+++ models.py
@@ -9,12 +9,18 @@ class NanogramSolution:
     def __init__(self, clues, solution):
         self.clues = clues
         self.solution = np.array(solution).reshape(len(clues.columns), len(clues.rows))
-        self.row_correctness = self.__get_row_correctness(self.solution, self.clues.rows)
-        self.column_correctness = self.__get_row_correctness(self.solution.T, self.clues.columns)
+        self.solution_clues = self.__get_clues(self.solution)
+        self.row_correctness = (self.clues.rows == self.solution_clues.rows)
+        self.column_correctness = (self.clues.columns == self.solution_clues.columns)
 
-    def __get_row_correctness(self, solution, cluesRows):
-        rows = np.array([self.__convert_line(row) for row in solution], dtype=object)
-        return cluesRows == rows
+    def __get_clues(self, solution):
+        return NanogramClues(
+            columns = self.__get_clues_for_one_orientation(self.solution.T),
+            rows = self.__get_clues_for_one_orientation(self.solution)
+        )
+
+    def __get_clues_for_one_orientation(self, solution):
+        return np.array([self.__convert_line(row) for row in solution], dtype=object)
 
     def __convert_line(self, line):
         result = []
