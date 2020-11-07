7c1ee50da9ee432d0e3bb038491ed31aec2a34ff
Jakub Cisło <cubix651@gmail.com>
2020-11-07 22:22:55 +0100

    Fitness with edit distance

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from models import NanogramSolution
 from data import CLUES
 from nanogram import NanogramGA
 from tqdm import tqdm
+import editdistance
 
-def fitness(chromosome, clues):
+def fitness1(chromosome, clues):
     nanogram_solution = NanogramSolution(clues, chromosome)
     return np.sum(nanogram_solution.row_correctness) + np.sum(nanogram_solution.column_correctness)
 
+def fitness2(chromosome, clues):
+    ns = NanogramSolution(clues, chromosome)
+    a = [editdistance.eval(x, y) for x, y in zip(ns.solution_clues.rows, clues.rows)]
+    b = [editdistance.eval(x, y) for x, y in zip(ns.solution_clues.columns, clues.columns)]
+    return -(sum(a)+sum(b))
+
 def main():
-    generations = 10
+    generations = 300
     nga = NanogramGA(
         CLUES,
-        fitness,
-        population_size=200,
+        fitness2,
+        population_size=1000,
         generations=generations,
         mutation_probability=0.05,
         elitism=True
